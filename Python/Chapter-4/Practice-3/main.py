b7384d2d4555a8ca3c908fc4add403a347b161c9
imWhs <dnjsgml1230@gmail.com>
2021-06-15 11:05:41 +0900

    Solved a new problem

--- Python/Chapter-4/Practice-3/main.py
+++ Python/Chapter-4/Practice-3/main.py
@@ -13,55 +13,60 @@ visited = [[0] * M for _ in range(N)]
 
 #(0, 0) 시작 지점 방문 처리
 visited[r][c] = 1
+result = 1
 
 #방향 별 이동 정보: 0 U, 1 R, 2 D, 3 L
 rotate = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-print(f"초기 방향: ({r + rotate[d][0]}, {c + rotate[d][1]})")
+def turn():
+    global d
+    d = (d - 1) % 4
 
-# 4 4
-# 1 1 0
-# 1 1 1 1
-# 1 0 0 1
-# 1 1 0 1
-# 1 1 1 1
-
-r_cnt = 0
-nd = d
+# 1단계: 현재 방향 d를 기준으로 왼쪽 방향으로 돌며 갈 곳 정한다.
 
 while 1:
-    print(f"현재 위치: ({r}, {c})")
-    #왼쪽 방향 회전 -> visited 확인 -> 가보지 않은 칸이라면 한 칸 전진 -> 가 본 칸이라면 왼쪽 방향 추가 회전
-    nd = (d - 1) % 4
-    r_cnt += 1
+    is_avail = False
 
-    #회전한 방향의 지점 좌표 계산
-    nr, nc = r + rotate[nd][0], c + rotate[nd][1]
-    # tmp = input()
-    print(f"회전 후 방향: ({nr}, {nc})")
+    for i in range(0, 3):
+        turn()
+        i += 1
+        nr, nc = r + rotate[d][0], c + rotate[d][1]
 
+        if nr < 0 or nc < 0 or nr > N or nc > M:
+            continue
 
-    #가 본 칸이거나 바다라면 다시 회전
-    if (visited[nr][nc] == 1) or (game_map[nr][nc] == 1):
-        print("- 가본 칸이거나 바다 ")
-        #이미 4 방향 모두 회전한 상태였다면, 한 칸 뒤로 후진 먼저 한 후 다시 회전해야 한다.
-        if r_cnt == 3:
-            print("  - 4 방향 모두 회전")
-            nr, nc = r - rotate[d][0], c - rotate[d][1]
-            #한 칸 뒤로 후진할 수 없는 경우 움직임을 멈춘다.
-            if nr < 0 or nc < 0 or nr > 4 or nc > 4:
-                break
-            r, c = nr, nc
-            r_cnt = 0
+        if game_map[nr][nc]:
+            continue
 
-        #다시 회전한다.
+        if visited[nr][nc]:
+            continue
+
+        r, c = nr, nc
+        visited[r][c] = 1
+        is_avail = True
+        result += 1
+        break
+
+    if is_avail:
         continue
 
-    #가보지 않은 칸이라면 해당 방향으로 한 칸 전진
-    r, c = nr, nc
-    visited[r][c] = 1
-    r_cnt = 0
-    d = nd
+    r, c = r - rotate[d][0], c - rotate[d][1]
+    if r < 0 or c < 0 or r > N or c > M or game_map[r][c] or visited[r][c]:
+        break
+
+
+print(result)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
